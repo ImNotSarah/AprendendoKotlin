5687f3281eeb71cdcf2c5f34abeada0ff715ef09
Sarah <sarahdejesuslima07@gmail.com>
2025-01-28 11:48:13 -0300

    Adicionado Aula Variaveis e constantes

---
+++ app/src/main/java/com/example/aprendendokotlindozero/VariaveisConstantes.kt
@@ -0,0 +1,26 @@
+package com.example.aprendendokotlindozero
+
+fun retornaNomeUsuario(){
+
+}
+
+class Usuario{
+
+}
+
+const val nomeUsuario = "Sarah" /* Variavel global */
+
+fun main(){
+
+    val nome = retornaNomeUsuario()
+
+    val numero1 = 20
+    val numero2 = 30
+    val resultado = numero1 + numero2
+
+    val numero: Int = 7
+
+    val usuario = Usuario() /* Instanciando Usuario */
+
+    println(nomeUsuario)
+}
