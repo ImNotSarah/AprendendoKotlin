e321e010eb7538104344a97028ad579860cf5141
Sarah <sarahdejesuslima07@gmail.com>
2025-02-01 17:40:48 -0300

    Adicionado Aula Funções Inline

---
+++ app/src/main/java/com/example/aprendendokotlindozero/FuncoesInline.kt
@@ -0,0 +1,10 @@
+package com.example.aprendendokotlindozero
+
+//fun subtrair() = println( 8 - 6)
+
+fun subtrair(): Int = 10 - 8
+
+fun main() {
+    val retorno = subtrair()
+    println(retorno)
+}
