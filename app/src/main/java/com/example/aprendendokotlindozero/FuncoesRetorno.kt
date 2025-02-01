a3178c5e18718e9fb89021f42bec473b83630466
Sarah <sarahdejesuslima07@gmail.com>
2025-02-01 17:24:45 -0300

    Adicionado Aula funções com e sem retorno

---
+++ app/src/main/java/com/example/aprendendokotlindozero/FuncoesRetorno.kt
@@ -0,0 +1,12 @@
+package com.example.aprendendokotlindozero
+
+fun somar(): Int{
+    return 10
+    //println(5 + 5)
+}
+
+
+fun main() {
+    val retorno = somar()
+    println(retorno * 2)
+}
--- app/src/main/java/com/example/aprendendokotlindozero/IntroducaoFuncoes.kt
+++ app/src/main/java/com/example/aprendendokotlindozero/IntroducaoFuncoes.kt
@@ -1,10 +1,12 @@
 package com.example.aprendendokotlindozero
 
 //Definição
+/*
 fun somar(){
     println(2 + 5)
 }
+*/
 
 fun main() {
-    somar() // chamada da função
+    //somar() // chamada da função
 }
